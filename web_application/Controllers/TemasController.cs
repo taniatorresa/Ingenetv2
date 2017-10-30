5961fb9c5952038f636100d45ecbe87c5a422379
RannFerii <josue_dragon_13@hotmail.com>
2017-10-30 08:53:04 -0600

    propuesta de formulario,cambio del logo

--- web_application/Controllers/TemasController.cs
+++ web_application/Controllers/TemasController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace MyAppWebStore.Controllers
 {
     public class TemasController : Controller
